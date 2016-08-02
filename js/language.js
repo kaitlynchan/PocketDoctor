93e968212c313e39b5c06355bc43e8918393ebf7
kaitlynchan <kaitlynchan@users.noreply.github.com>
2016-08-02 13:51:58 -0700

    uploaded

---
+++ js/language.js
@@ -0,0 +1,41 @@
+var x = document.cookie;
+var i=1;
+
+var current_aud;
+var y;
+  if (x == "Language=Hindi"){ 
+    
+    for(i=1;i<16;i++){
+      
+      current_aud="aud"+String(i);
+      y= document.getElementById(current_aud).getAttribute("data-language");
+      if (y=="Hindi") {
+        document.getElementById(current_aud).style.display='block';
+      };
+    }
+  }
+
+  else if (x == "Language=French"){ 
+    
+    for(i=1;i<16;i++){
+      
+      current_aud="aud"+String(i);
+      y= document.getElementById(current_aud).getAttribute("data-language");
+      if (y=="French") {
+        document.getElementById(current_aud).style.display='block';
+      };
+    }
+  }
+
+  else { 
+    for(i=1;i<16;i++){
+      
+      current_aud="aud"+String(i);
+      y= document.getElementById(current_aud).getAttribute("data-language");
+      if (y=="English") {
+        document.getElementById(current_aud).style.display='block';
+      };
+    }
+  }
+  
+  
